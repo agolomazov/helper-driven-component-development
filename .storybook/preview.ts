2ea72f435e1ebcc91c2b787096539f2d0d53ff44
Anton Golomazov <golomazov-tosha2012@yandex.ru>
2024-08-14 09:00:25 +0300

    Add viewports settings for storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -15,6 +15,31 @@ const preview: Preview = {
         { name: 'bright', value: '#f30' },
       ],
     },
+    viewport: {
+      viewports: {
+        kindleFire2: {
+          name: 'Kindle Fire 2',
+          styles: {
+            width: '600px',
+            height: '963px',
+          },
+        },
+        kindleFireHD: {
+          name: 'Kindle Fire HD',
+          styles: {
+            width: '533px',
+            height: '801px',
+          },
+        },
+        iphone5: {
+          name: 'Iphone 5',
+          styles: {
+            width: '320px',
+            height: '568px',
+          },
+        },
+      },
+    },
   },
 };
 
